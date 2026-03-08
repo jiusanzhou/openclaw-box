89549929cd220d5ac2ffb6da1adb2129a47ce08d
Xiaolu Zhou <hi@zoe.im>
2026-03-08 21:25:54 +0800

    fix: disable right-click context menu

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
 
+
+// 禁用右键菜单
+document.addEventListener("contextmenu", (e) => e.preventDefault());
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
